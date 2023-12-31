c4f527001e6a7e4e9ae7fccc6291cedd07e20cf1
Ravi Pandey <imravipanday@gmail.com>
2024-01-01 03:36:05 +0530

    Project is ready to use

---
+++ assets/JS/app.js
@@ -0,0 +1,46 @@
+// share page
+function submitForm() {
+  // Get values from the form
+  var fromName = document.getElementById("ownName").value;
+  var toName = document.getElementById("sendName").value;
+
+  // Construct the URL with parameters
+  var url =
+    "index.html?from=" +
+    encodeURIComponent(fromName) +
+    "&to=" +
+    encodeURIComponent(toName);
+
+  // Redirect to the URL
+  window.location.href = url;
+}
+
+// gretting page
+function getParameterByName(name) {
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(window.location.href);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+if (!fromParameter || !toParameter) {
+  window.location.href = "index.html?from=Ravi%20pandey&to=shweta%20bandawane";
+}
+
+var fromParameter = getParameterByName("to");
+var toParameter = getParameterByName("from");
+
+document.addEventListener("DOMContentLoaded", function () {
+  let nameFrom = document.querySelector(".from");
+  let nameTo = document.querySelector(".to");
+
+  if (nameFrom) {
+    nameFrom.innerText = fromParameter;
+  }
+
+  if (nameTo) {
+    nameTo.innerText = toParameter;
+  }
+});
